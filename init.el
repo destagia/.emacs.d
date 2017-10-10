e1544022df5cbafafc7bc0a2d8ee3a5f9f0c6257
Shohei Miyashita <shohei.miyashita@dena.com>
2017-10-10 16:20:23 +0900

    fix: font faces

--- init.el
+++ init.el
@@ -58,3 +58,16 @@
     (set-frame-parameter nil 'alpha 90) ;透明度
     ))
 
+;; Font
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "#263238" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "PT Mono")))))
