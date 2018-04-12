7dac68f3a5921c8f54675bff6f2c2e76f22cf16b
Shohei Miyashita <shohei.miyashita@dena.com>
2018-04-12 10:45:06 +0900

    fix

--- init.el
+++ init.el
@@ -17,6 +17,10 @@
 (transient-mark-mode t)
 ;; 空白を可視化
 ;; (global-whitespace-mode 1)
+;; 自動でファイルをリロード
+(global-auto-revert-mode t)
+
+(setq default-tab-width 4)
 
 
 ;; メニューバーを非表示
@@ -34,6 +38,10 @@
 (require 'helm-config)
 (helm-mode t)
 (global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x r b") 'helm-filtered-bookmarks)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-set-key (kbd "C-x c") 'helm-find)
 
 ;; Auto Complete
 (require 'auto-complete-config)
@@ -64,10 +72,35 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   (quote
+	(yaml-mode dockerfile-mode company-go flycheck markdown-mode material-theme helm auto-complete))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "#263238" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "nil" :family "PT Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "#263238" :foreground "#ffffff" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "nil" :family "PT Mono")))))
+
+;; Goのパスを通す
+(add-to-list 'exec-path (expand-file-name "/Users/shohei.miyashita/.goenv/shims"))
+
+;; go get で入れたツールのパスを通す
+(add-to-list 'exec-path (expand-file-name "/Users/shohei.miyashita/.go"))
+
+;; 必要なパッケージのロード
+(require 'go-mode)
+(require 'company-go)
+
+;; 諸々の有効化、設定
+(add-hook 'go-mode-hook 'company-mode)
+(add-hook 'go-mode-hook 'flycheck-mode)
+(add-hook 'go-mode-hook (lambda()
+           (add-hook 'before-save-hook' 'gofmt-before-save)
+           (local-set-key (kbd "M-.") 'godef-jump)
+           (set (make-local-variable 'company-backends) '(company-go))
+           (company-mode)
+           (setq indent-tabs-mode nil)    ; タブを利用
+           (setq c-basic-offset 4)        ; tabサイズを4にする
+           (setq tab-width 4)))
+
